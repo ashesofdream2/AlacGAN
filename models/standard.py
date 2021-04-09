4324c9e4d6aa76493485ec8615d410ad25b66158
ashesofdream <53961395+ashesofdream@users.noreply.github.com>
2021-04-09 22:03:53 +0800

    Update standard.py

--- models/standard.py
+++ models/standard.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as M
 
-VGG16_PATH = '../input/sketch-gan/vgg16-397923af.pth'
+VGG16_PATH = '../input/pytorch-model-zoo/vgg16-397923af.pth'
 I2V_PATH = '../input/sketch-gan/i2v.pth'
 
 

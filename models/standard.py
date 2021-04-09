f70e129af97a16c3f93798afb1a8e3f6370c83ac
ashesofdream <53961395+ashesofdream@users.noreply.github.com>
2021-04-09 21:18:54 +0800

    Update standard.py

--- models/standard.py
+++ models/standard.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as M
 
-VGG16_PATH = 'resources/vgg16-397923af.pth'
-I2V_PATH = 'resources/i2v.pth'
+VGG16_PATH = '../input/sketch-gan/vgg16-397923af.pth'
+I2V_PATH = '../input/sketch-gan/i2v.pth'
 
 
 class ResNeXtBottleneck(nn.Module):

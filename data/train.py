49af5732a58702bd39ab57d3d970ae987903835c
ashesofdream <53961395+ashesofdream@users.noreply.github.com>
2021-04-12 21:10:26 +0800

    Update train.py

--- data/train.py
+++ data/train.py
@@ -251,7 +251,8 @@ def CreateDataLoader(config):
     ])
 
     VTrans = transforms.Compose([
-        RandomSizedCrop(config.image_size // 4, Image.NEAREST),
+        #RandomSizedCrop(config.image_size // 4, Image.NEAREST),
+        transforms.Resize(config.imge_size //4 ,Image.NEARST),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])

c2db1a4bbff8d3c9c2a5c96ff2afd867d6d03124
ashesofdream <53961395+ashesofdream@users.noreply.github.com>
2021-05-04 01:17:56 +0800

    Update train.py

--- data/train.py
+++ data/train.py
@@ -231,14 +231,14 @@ class ImageFolder(data.Dataset):
     def __getitem__(self, index):
         fname = self.imgs[index]
         Cimg = color_loader(os.path.join(self.root, 'illustrations_remake', fname))
-        kind = random.randint(1,3)
+        kind = random.randint(3,5)
 #         if kind == 1:
 #             thick = str(random.randint(3,5))
 #             Simg = sketch_loader(os.path.join(self.root, '0.'+thick+'XDoG', fname))
 #         elif kind == 2:
 #             Simg = sketch_loader(os.path.join(self.root, 'sketch_kersa_torch', fname))
             #Simg = sketch_loader(os.path.join(self.root, 'illustrations_resized_final/illustrations_resized_final', fname))
-        Simg = sketch_loader(os.path.join(self.root, '0.'+thick+'XDoG', fname))
+        Simg = sketch_loader(os.path.join(self.root, '0.'+kind+'XDoG', fname))
         Cimg, Simg = RandomCrop(512)(Cimg, Simg)
         if random.random() < 0.5:
             Cimg, Simg = Cimg.transpose(Image.FLIP_LEFT_RIGHT), Simg.transpose(Image.FLIP_LEFT_RIGHT)

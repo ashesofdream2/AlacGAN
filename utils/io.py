37a9b45ae56a87d37291fe4840448c0b5dc54fa7
ashesofdream2 <83710547+ashesofdream2@users.noreply.github.com>
2021-05-06 23:30:17 +0800

    Update io.py

--- utils/io.py
+++ utils/io.py
@@ -52,7 +52,7 @@ def load_state(path, netG, netD, optimizerG, optimizerD,load_fid=False):
         for k in missing_keys:
             print(f'caution: missing netD keys from checkpoint {path}: {k}')
         
-        if load_fid == True and checkpoint.has_key("iter_record"):
+        if load_fid == True and "iter_record" in checkpoint:
             iter_record = checkpoint['iter_record']
             fid_record  = checkpoint['fid_record']
             print("load fid_record successfully")
